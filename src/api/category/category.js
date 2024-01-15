39045186bb893f38ee4a0658906cbeb3c2814e10
Dracowyn <dracowyn@outlook.com>
2024-01-15 17:29:44 +0800

    完善学术页面

--- src/api/category/category.js
+++ src/api/category/category.js
@@ -1,6 +1,12 @@
 import {POST} from "../../services/request";
 
 const Category = {
+	index(data = {}) {
+		return POST({
+			url: '/category/index',
+			params: data,
+		})
+	},
 	getHot(data = {}) {
 		return POST({
 			url: '/category/hot',
--- src/views/category/Index.js
+++ src/views/category/Index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Footer from "../../components/common/Footer";
-import {List, NavBar, PullRefresh, Search, Sticky} from "react-vant";
+import {Empty, Image, List, NavBar, PullRefresh, Search, Sticky, Swiper, Toast} from "react-vant";
 import {AppsO} from "@react-vant/icons";
+import category from "../../api/category/category";
+import {NavLink} from "react-router-dom";
 
 const Index = () => {
 	const [hotList, setHotList] = React.useState([]);
@@ -9,24 +11,102 @@ const Index = () => {
 	const listRef = React.useRef(null);
 	const [finished, setFinished] = React.useState(false);
 
-	const onRefresh = () => {
+	const [page, setPage] = React.useState(1);
+	const [limit, setLimit] = React.useState(10);
+	const categoryData = React.useRef([]);
 
+	const isMounted = React.useRef(true);
+
+	const onRefresh = () => {
+		setFinished(false);
+		setPage(1);
+		setCategoryList([]);
+		categoryData.current = [];
+
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				Toast.info('刷新成功');
+				getCategoryData().then(r => r);
+				resolve(true);
+			}, 1500)
+		})
 	}
 
 	const onLoadRefresh = () => {
-
+		setFinished(true);
+		getCategoryData().then(r => r);
 	}
 
 	const Items = () => {
-
+		if (categoryList.length > 0) {
+			return categoryList.map(item => {
+				return (
+					<li key={item.id}>
+						<NavLink to={'/category/category/info?id=' + item.id} className={'a_blok'}>
+							<img src={item.image_cdn} alt={''}/>
+							<p>{item.name}</p>
+							<span>{item.createtime_text}</span>
+						</NavLink>
+					</li>
+				)
+			})
+		} else {
+			return <Empty description={'暂无数据'}/>
+		}
 	}
 
 
-	React.useState(() => {
+	React.useEffect(() => {
 		const getHotData = async () => {
+			let result = await category.getHot();
 
+			if (result && result.code === 1) {
+				setHotList(result.data);
+			}
 		}
+		getHotData().then(r => r);
+
+		return () => {
+			isMounted.current = false;
+		};
 	}, []);
+
+	const getCategoryData = async () => {
+		let data = {
+			page,
+			limit,
+		}
+
+		let result = await category.index(data);
+
+		if (result && result.code === 1) {
+			let count = result.data.count;
+			categoryData.current = categoryData.current.concat(result.data.list);
+			setCategoryList(categoryData.current);
+
+			if (categoryData.current.length >= count) {
+				setFinished(true);
+				return;
+			}
+
+			setPage(page + 1);
+			setFinished(false);
+		}
+	}
+
+	const HotSwiper = () => {
+		if (hotList.length > 0) {
+			return (
+				<Swiper>
+					{hotList.map(item => (
+						<Swiper.Item key={item.id}>
+							<Image lazyLoad src={item.image_cdn}/>
+						</Swiper.Item>
+					))}
+				</Swiper>
+			)
+		}
+	}
 
 	return (
 		<>
@@ -39,12 +119,12 @@ const Index = () => {
 			</Sticky>
 
 			<div className={'banner_shouy'}>
-
+				<HotSwiper/>
 			</div>
 
 			{/* 列表 */}
-			<PullRefresh onRefresh={onRefresh} onLoadRefresh={() => listRef.current?.check()}>
-				<List finished={finished} ref={listRef} onLoad={onLoadRefresh()}>
+			<PullRefresh onRefresh={onRefresh} onRefreshEnd={() => listRef.current?.check()}>
+				<List finished={finished} ref={listRef} onLoad={onLoadRefresh}>
 					<div className={'lest_xs'}>
 						<ul>
 							<Items/>
